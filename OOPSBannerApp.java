430be4f09476eb45fbddec295dfb0d90fd199b16
Vithya Krishnan R A <vithyakrish1402@gmail.com>
2026-02-17 22:37:12 +0530

    UC1 Created

---
+++ OOPSBannerApp.java
@@ -0,0 +1,21 @@
+/**
+ * OOPSBannerApp
+ *
+ * This program prints the literal text "OOPS" to the console.
+ *
+ * @author VithyaKrishnan
+ * @version 1.0
+ */
+public class OOPSBannerApp {
+
+    /**
+     * Main method - Entry point of the application
+     * @param args Command-line arguments
+     */
+    public static void main(String[] args) {
+
+        // Print OOPS to console
+        System.out.println("OOPS");
+
+    }
+}
