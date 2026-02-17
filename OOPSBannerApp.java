eba3185fef1636878f1a4361758e28066dfbdc84
Vithya Krishnan R A <vithyakrish1402@gmail.com>
2026-02-17 22:53:54 +0530

    done

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,10 +1,11 @@
 /**
  * OOPSBannerApp
  *
- * This program prints the literal text "OOPS" to the console.
+ * UC2: Render OOPS as Banner using Print Statements
+ * Displays OOPS in banner format using * and spaces.
  *
- * @author VithyaKrishnan
- * @version 1.0
+ * @author Vithya Krishnan
+ * @version 2.0
  */
 public class OOPSBannerApp {
 
@@ -14,8 +15,25 @@ public class OOPSBannerApp {
      */
     public static void main(String[] args) {
 
-        // Print OOPS to console
-        System.out.println("OOPS");
+        // Line 1
+        System.out.println(" ******* " + " ******* " + " ******* " + " ******* ");
 
+        // Line 2
+        System.out.println(" *     * " + " *     * " + " *     * " + " *       ");
+
+        // Line 3
+        System.out.println(" *     * " + " *     * " + " *     * " + " *       ");
+
+        // Line 4
+        System.out.println(" *     * " + " *     * " + " ******* " + " ******* ");
+
+        // Line 5
+        System.out.println(" *     * " + " *     * " + " *       " + "       * ");
+
+        // Line 6
+        System.out.println(" *     * " + " *     * " + " *       " + "       * ");
+
+        // Line 7
+        System.out.println(" ******* " + " ******* " + " *       " + " ******* ");
     }
 }
